d4bd152001b08cc6dd7b1674db335d6e9c99eb37
TaekedeHaan <Taeke.dehaan@live.nl>
2017-06-14 22:51:43 +0200

    Now working for both parameters

--- Spline_Generator/runSplineGeneratorTest.m
+++ Spline_Generator/runSplineGeneratorTest.m
@@ -5,10 +5,12 @@ clear all
 sim('SplineGeneratorTest.slx') 
 
 angles1 = f1.Data(1,:);
-%angles2 = f2.Data(1,:);
+angles2 = f2.Data(1,:);
 figure
 plot(0:0.1:99.9,angles1,'o')
 hold on
 plot(100:0.1:199.9,angles1,'o')
-%hold on
-%plot(angles2)
+hold on
+plot(0:0.1:99.9,angles2,'o')
+hold on
+plot(100:0.1:199.9,angles2,'o')
