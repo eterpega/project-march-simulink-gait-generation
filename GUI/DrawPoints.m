bd793e8709b90f141d7c155c96d6a9d7cde64093
l2late <lucadelaat@5070AB1A.static.ziggozakelijk.nl>
2017-06-18 19:01:41 +0200

    Text field updates when dragging points and points are sorted on ascending X coordinate

--- GUI/DrawPoints.m
+++ GUI/DrawPoints.m
@@ -1,14 +1,17 @@
 function DrawPoints(hObject, eventdata, handles)
+guidata(hObject, handles);
 
 %Define Graph type
 switch hObject
     case handles.GraphQKnee
+        type=1;
         GraphHandle=handles.GraphQKnee;
         PhaseHandle=handles.KeyEventPhaseKnee;
         YHandle=handles.KeyEventQKnee;
         GraphHandle.XLim=handles.KneeXLim;
         GraphHandle.YLim=handles.KneeYLim;
     case handles.GraphX
+        type=2;
         GraphHandle=handles.GraphX;
         PhaseHandle=handles.KeyEventPhaseX;
         YHandle=handles.KeyEventX;
@@ -20,10 +23,12 @@ end
 
 %Clear figure of previous points
 cla(GraphHandle);
-
-%Get data from values entered in GUI, assign to x and y
+%Get data from values in GUI, assign to x and y
 x=str2num(get(PhaseHandle,'String'));
 y=str2num(get(YHandle,'String'));
+%PhaseHandle=num2str(x);
+%YHandle=num2str(y);
+
 
 %Check same that x and y have same length
 if length(x)~=length(y)
@@ -32,12 +37,14 @@ else
     %Set draggable points on figure and limit them to axis of figure
     fcn=makeConstrainToRectFcn('impoint',GraphHandle.XLim,GraphHandle.YLim);
         for n=1:length(x);
-        h = impoint(GraphHandle,x(n),y(n));
-        set(h, 'Tag', strcat('h',num2str(n)));
-        setPositionConstraintFcn(h,fcn);
-        addNewPositionCallback(h,@(varargin)UpdatePosText(h));
+        h(n) = impoint(GraphHandle,x(n),y(n));
+        setPositionConstraintFcn(h(n),fcn);
+        GraphHandle.UserData.Points{n}=h(n);
+        GraphHandle.UserData.Position{n}=getPosition(h(n));
+        guidata(hObject,handles);
+        addNewPositionCallback(h(n),@(varargin)UpdatePosText(h(n),handles));
         end
+guidata(hObject,handles);
 end
-guidata(hObject, handles);
 
 
--- GUI/UpdatePosText.m
+++ GUI/UpdatePosText.m
@@ -1,9 +1,55 @@
-function UpdatePosText(h)
-pos = round(getPosition(h),2,'significant');
-PointNumber=char(get(h, 'Tag')); %Get tag of impoint for Order
-PointNumber=PointNumber(2:2); % take only 
-PointNumber=str2num(PointNumber);
-setString(h,[num2str(pos(1)) ', ' num2str(pos(2))]);
-wait(h);
-setString(h,'');
+function UpdatePosText(h,handles)
+pos = getPosition(h);
+graph=get(h,'parent');
+
+switch get(graph,'Tag')
+    case 'GraphQKnee'
+        GraphHandle=handles.GraphQKnee;
+        PhaseHandle=handles.KeyEventPhaseKnee;
+        YHandle=handles.KeyEventQKnee;
+        GraphHandle.XLim=handles.KneeXLim;
+        GraphHandle.YLim=handles.KneeYLim;
+    case 'GraphX'
+        GraphHandle=handles.GraphX;
+        PhaseHandle=handles.KeyEventPhaseX;
+        YHandle=handles.KeyEventX;
+        GraphHandle.XLim=handles.XXLim;
+        GraphHandle.YLim=handles.XYLim;
+    otherwise 
+        error('ERROR: Inputtype not allowed')
+end
+for n=1:size(GraphHandle.UserData.Points,2)
+    hother=GraphHandle.UserData.Points{n};
+    position(n,:)=getPosition(hother);
+end
+
+%Sort points on ascending X position
+position = sortrows(position,1);
+%Transpose position matrix
+position = position';
+
+%initialise strings
+PhaseString='';
+YString='';
+
+%write coordinates to strings
+for n=1:length(position(1,:))
+    PhaseString=[PhaseString,num2str(position(1,n),'%2.1f'),', '];
+    YString=[YString,num2str(position(2,n),'%2.1f'),', '];
+end
+
+%Remove last comma
+PhaseString=PhaseString(1:end-2);
+YString=YString(1:end-2);
+
+%Put brackets around
+PhaseString=strcat('[',PhaseString,']');
+YString=strcat('[',YString,']');
+
+%Set property equal to string
+set(PhaseHandle,'String',PhaseString);
+set(YHandle,'String',YString);
+
+%Update Handles
+guidata(graph,handles);
 end
