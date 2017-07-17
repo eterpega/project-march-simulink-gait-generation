c416744b8606e15dea80a79f715cab81dd577680
l2late <lucadelaat@gmail.com>
2017-07-17 18:33:22 +0200

    generated KeyEventData and Gait vectors for StandUp and SitDown. Added new folders for this data

--- Libraries/GUI/GUIDEKeyEventGen.m
+++ Libraries/GUI/GUIDEKeyEventGen.m
@@ -22,7 +22,7 @@ function varargout = GUIDEKeyEventGen(varargin)
 
 % Edit the above text to modify the response to help GUIDEKeyEventGen
 
-% Last Modified by GUIDE v2.5 09-Jul-2017 20:46:07
+% Last Modified by GUIDE v2.5 17-Jul-2017 15:54:24
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -614,3 +614,28 @@ angleHip_RPM, angleKnee_RPM, angleHip_rads2, angleKnee_rads2, time, samplePointA
 = compute_splines(handles, selected);
 detail_plots(phase, angleHip_deg, angleKnee_deg, x.x, y.y,...
 angleHip_RPM, angleKnee_RPM, angleHip_rads2, angleKnee_rads2, time, samplePointAmount);
+
+
+% --- Executes on selection change in GaitTypeList.
+function GaitTypeList_Callback(hObject, eventdata, handles)
+% hObject    handle to GaitTypeList (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global gaitType
+selected=getappdata(handles.SelectionList,'selected');
+contents = cellstr(get(hObject,'String'));
+gaitType=contents{get(hObject,'Value')}
+compute_splines(handles, selected);
+
+
+% --- Executes during object creation, after setting all properties.
+function GaitTypeList_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to GaitTypeList (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
