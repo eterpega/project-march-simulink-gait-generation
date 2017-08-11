5905486ffc00f4d010583ee49233a2b6da61cf22
l2late <lucadelaat@gmail.com>
2017-08-11 16:03:08 +0200

    set path at launch of GUI

--- GUIDEKeyEventGen.m
+++ GUIDEKeyEventGen.m
@@ -55,7 +55,6 @@ function GUIDEKeyEventGen_OpeningFcn(hObject, eventdata, handles, varargin)
 % varargin   command line arguments to GUIDEKeyEventGen (see VARARGIN)
 
 % Choose default command line output for GUIDEKeyEventGen
-cd 
 %set pwd to dir of this script and then add pwd to path
 [PATHSTR,~,~] = fileparts(mfilename('fullpath'));
 cd(PATHSTR);
